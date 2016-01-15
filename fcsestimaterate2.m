165fa4f96120efe1f49d5b9bb083e68daea62b62
Hua, Bo <bflyingbox@gmail.com>
2016-01-15 16:29:47 -0500

    Add code to roughly estimate culture density

---
+++ fcsestimaterate2.m
@@ -0,0 +1,31 @@
+function [ rate ] = fcsestimaterate2( dataT, varargin )
+%UNTITLED12 Summary of this function goes here
+%   Detailed explanation goes here
+
+% dataT = sort(dataT, ascend);
+
+if length(dataT) < 10
+    rate = NaN;
+    return
+end
+
+rangeMin = 0.3;
+rangeMax = 0.7;
+
+% nCount = length(dataT);
+% nCountRange = ceil([rangeMin, rangeMax] * nCount);
+% 
+% dataTFilter = dataT([nCountRange(1):nCountRange(2)]);
+
+dataTFilter = dataT;
+
+dataTMax = max(dataTFilter);
+dataTMin = min(dataTFilter);
+
+dataThreMin = dataTMin + rangeMin*(dataTMax-dataTMin);
+dataThreMax = dataTMin + rangeMax*(dataTMax-dataTMin);
+
+counts = length(find(dataTFilter > dataThreMin & dataTFilter < dataThreMax));
+rate = counts/(dataThreMax - dataThreMin);
+
+end
