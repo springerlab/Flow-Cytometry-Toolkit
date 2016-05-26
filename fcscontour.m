88ec158f1592262d60d20ec3277ef667252bcf18
Jue Wang <cremeglace@gmail.com>
2016-05-26 11:06:22 -0400

    simplify fcsdensity, add fcscontour

---
+++ fcscontour.m
@@ -0,0 +1,36 @@
+function out = fcscontour(X,Y,varargin)
+%FCSCONTOUR makes a 2D contour plot of flow cytometry data.
+%
+% Created 20160526 JW
+p = inputParser;
+addParameter(p,'xi',[],@isnumeric);
+addParameter(p,'yi',[],@isnumeric);
+addParameter(p,'smrad',2,@isnumeric);
+addParameter(p,'minp',1e-5,@isnumeric);
+addParameter(p,'ncontours',5,@isnumeric);
+
+parse(p,varargin{:});
+xi = p.Results.xi;
+yi = p.Results.yi;
+smrad = p.Results.smrad;
+minp = p.Results.minp;
+ncontours = p.Results.ncontours;
+
+if isempty(xi)
+    xi = linspace(min(X),max(X),50);
+end
+if isempty(yi)
+    yi = linspace(min(Y),max(Y),50);
+end
+
+d = hist3([X Y],{xi yi})./numel(X);
+
+mopts = {'.','color',[.5 .5 .5],'markersize',3};
+plot(X,Y,mopts{:});
+hold all
+
+d = imgaussfilt(d,smrad);
+d(d <= minp) = nan;
+d = log10(d);
+
+contour(xi,yi,d',ncontours,'color','r');
