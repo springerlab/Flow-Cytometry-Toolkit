2896ec6da5c11fe1ace9d21e4e6f55fbbcb9e23a
Jue Wang <cremeglace@gmail.com>
2016-08-01 19:10:31 -0400

    tweak fcscontour

--- fcscontour.m
+++ fcscontour.m
@@ -1,13 +1,17 @@
-function out = fcscontour(X,Y,varargin)
+function [C,H] = fcscontour(X,Y,varargin)
 %FCSCONTOUR makes a 2D contour plot of flow cytometry data.
 %
-% Created 20160526 JW
+% 
+% Created / Last Updated
+% 20160526 / 20160721
 p = inputParser;
 addParameter(p,'xi',[],@isnumeric);
 addParameter(p,'yi',[],@isnumeric);
-addParameter(p,'smrad',2,@isnumeric);
+addParameter(p,'smrad',1,@isnumeric);
 addParameter(p,'minp',1e-5,@isnumeric);
 addParameter(p,'ncontours',5,@isnumeric);
+addParameter(p,'color','r',@(x) true);
+addParameter(p,'scatterplot',{'.','color',[.7 .7 .7],'markersize',3},@iscell);
 
 parse(p,varargin{:});
 xi = p.Results.xi;
@@ -15,6 +19,8 @@ yi = p.Results.yi;
 smrad = p.Results.smrad;
 minp = p.Results.minp;
 ncontours = p.Results.ncontours;
+color = p.Results.color;
+scatterplot = p.Results.scatterplot;
 
 if isempty(xi)
     xi = linspace(min(X),max(X),50);
@@ -25,12 +31,13 @@ end
 
 d = hist3([X Y],{xi yi})./numel(X);
 
-mopts = {'.','color',[.5 .5 .5],'markersize',3};
-plot(X,Y,mopts{:});
-hold all
+if ~isempty(scatterplot)
+    plot(X,Y,scatterplot{:});
+    hold all
+end
 
 d = imgaussfilt(d,smrad);
 d(d <= minp) = nan;
 d = log10(d);
 
-contour(xi,yi,d',ncontours,'color','r');
+[C,H]=contour(xi,yi,d',ncontours,'color',color);
